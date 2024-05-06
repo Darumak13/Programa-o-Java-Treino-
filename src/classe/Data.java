739c70ab8550180f3d880d208fb369960fc63736
Daniels <danielvictor65.dv@gmail.com>
2024-05-06 17:22:46 -0300

    Estudos Ex Java

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -1,8 +1,24 @@
 package classe;
 
 public class Data {
-	
+
 	int Dia;
 	int Mes;
 	int Ano;
+	
+	Data(int DiaInicial, int MesInicial, int AnoInicial){
+		Dia =  DiaInicial;
+		Mes = MesInicial;
+		Ano = AnoInicial;
+	}
+	
+	Data(){
+		Dia = 1;
+		Mes = 1;
+		Ano = 1970;
+	}
+	
+	String obterDataFormatada() {
+		return String.format("%d/%d/%d", Dia, Mes, Ano);
+	}
 }
