1e4d96db2011dee1c0f0083c1bb4fa9fa8fd6558
Daniels <danielvictor65.dv@gmail.com>
2024-05-15 17:14:43 -0300

    Estudos Ex Java

---
+++ src/oo/composicao/desafio/Cliente.java
@@ -0,0 +1,15 @@
+package oo.composicao.desafio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+	
+	String nome;
+	List<Compra> compra = new ArrayList<>();
+	
+	void obterValorTotal(){
+		
+	}
+	
+}
